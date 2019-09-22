23d7854db8c0b2a8d0b01a00493cd97f434d88ba
Gyeongsik Kim <admin@gyungdal.cc>
2019-09-22 18:09:49 +0900

    주변에 뿌려줄 객체 추가 테스트용 뷰 추가

--- Madome/App.xaml.cs
+++ Madome/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Madome
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Views.Test();
         }
 
         protected override void OnStart()
---
+++ Madome/Views/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Madome.Views
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+        }
+    }
+}
