6f7a1d88a1a6e2f6d37775aa989d068928d6fbc2
Gyeongsik Kim <admin@gyungdal.cc>
2019-11-24 15:34:45 +0900

    Json 파서 로직 패치

--- Madome/Struct/Book.cs
+++ Madome/Struct/Book.cs
@@ -1,25 +1,65 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Madome.Enum.API;
 using Madome.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Madome.Struct {
 	public class Book : ObservableObject {
+
+		[JsonProperty(PropertyName = "id")]
 		public int Id { get; set; }
+		[JsonProperty(PropertyName = "title")]
 		public string Title { get; set; }
+
+		[JsonProperty(PropertyName = "group")]
+		public string[] Group { get; set; }
+
+		[JsonProperty(PropertyName = "characters")]
+		public string[] Characters { get; set; }
+
+		[JsonProperty(PropertyName = "artists")]
+		public string[] Artists { get; set; }
+
+		[JsonProperty(PropertyName = "series")]
+		public string[] Series { get; set; }
+
+		[JsonProperty(PropertyName = "tags")]
+		public string[] Tags { get; set; }
+
+		[JsonProperty(PropertyName = "type")]
+		public string Type { get; set; }
+
+		[JsonProperty(PropertyName = "language")]
+		public string Language { get; set; }
+
+		[JsonProperty(PropertyName = "page_count")]
+		public int PageCount { get; set; }
+
+		[JsonProperty(PropertyName = "created_at")]
+		public string CreateAt { get; set; }
+
 		public string Thumb {
 			get {
 				return APIHelper.Instance.Url + String.Format(RequestType.GET_BOOK_IMAGE.GetString(), Id, Images[0]);
 			}
 		}
-		public string[] Images { get;set; }
-		public string[] Group { get; set; }
-		public string[] Characters { get; set; }
-		public string[] Artists { get; set; }
-		public string[] Series { get; set; }
-		public string[] Tags { get; set; }
-		public string Type { get; set; }
-		public string Language { get; set; }
-		public int PageCount { get; set; }
-		public DateTime CreateAt { get; set; }
+		public string[] Images { get; set; }
+
+		public void ImageUpdate() {
+			Task.Run(async () => {
+				HttpResponse imageResponse = APIHelper.Instance.Get(String.Format(RequestType.GET_BOOK_IMAGE_LIST.GetString(), Id));
+				JArray images = (Newtonsoft.Json.Linq.JArray)imageResponse.Body["items"];
+				int index = 0;
+				Images = new string[images.Count];
+				foreach (string image in images) {
+					Images[index] = image;
+					index += 1;
+				}
+				OnPropertyChanged("Images");
+				OnPropertyChanged("Thumb");
+			});
+		}
 	}
 }
--- Madome/ViewModels/Library/BookListViewModel.cs
+++ Madome/ViewModels/Library/BookListViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using Madome.Enum.API;
 using Madome.Helpers;
 using Madome.Struct;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 
@@ -22,7 +23,7 @@ namespace Madome.ViewModels.Library {
 			Page = 0;
 			Books = new List<Book>();
 			LoadNextPageCommnad = new RelayCommand(async() => {
-				LoadingNext();
+				await LoadingNext();
 			});
 			RefreshCommand = new RelayCommand(async () => {
 				Page = 0;
@@ -38,73 +39,25 @@ namespace Madome.ViewModels.Library {
 			jObject.Add("offset", 25);
 			jObject.Add("page", Page);
 			HttpResponse response = APIHelper.Instance.Get(Enum.API.RequestType.GET_BOOKS, jObject);
+			//DateTime.Parse(NewBook["created_at"].ToString(), null, System.Globalization.DateTimeStyles.RoundtripKind),
 			switch (response.Code) {
 				case System.Net.HttpStatusCode.OK: {
-						JArray NewBooks = (JArray)response.Body["items"];
-						foreach (JObject NewBook in NewBooks) {
-							Book book = new Book {
-								Id = Convert.ToInt32(NewBook["id"]),
-								Title = NewBook["title"].ToString(),
-								Type = NewBook["type"].ToString(),
-								Language = NewBook["language"].ToString(),
-								PageCount = Convert.ToInt32(NewBook["page_count"]),
-								CreateAt = DateTime.Parse(NewBook["created_at"].ToString(), null, System.Globalization.DateTimeStyles.RoundtripKind),
-							};
-							HttpResponse imageResponse= APIHelper.Instance.Get(String.Format(RequestType.GET_BOOK_IMAGE_LIST.GetString(), book.Id));
-							JArray images = (Newtonsoft.Json.Linq.JArray)imageResponse.Body["items"];
-							int index = 0;
-							book.Images = new string[images.Count];
-							foreach (string image in images) {
-								book.Images[index] = image;
-								index += 1;
-							}
-							index = 0;
-							JArray groups = (JArray)NewBook["group"];
-							book.Group = new string[groups.Count];
-							foreach(string group in groups) {
-								book.Group[index] = group;
-								index += 1;
-							}
-							index = 0;
-							JArray characters = (JArray)NewBook["characters"];
-							book.Characters = new string[characters.Count];
-							foreach (string character in characters) {
-								book.Characters[index] = character;
-								index += 1;
-							}
-							index = 0;
-							JArray artists = (JArray)NewBook["artists"];
-							book.Artists = new string[artists.Count];
-							foreach (string artist in artists) {
-								book.Artists[index] = artist;
-								index += 1;
-							}
-							index = 0;
-							JArray series = (JArray)NewBook["series"];
-							book.Series = new string[series.Count];
-							foreach (string serie in series) {
-								book.Series[index] = serie;
-								index += 1;
-							}
-							index = 0;
-							JArray tags = (JArray)NewBook["tags"];
-							book.Tags = new string[tags.Count];
-							foreach (string tag in tags) {
-								book.Tags[index] = tag;
-								index += 1;
-							}
-							Books.Add(book);
-							Debug.WriteLine(Books.Count.ToString());
-							OnPropertyChanged("Books");
-						}
-						break;
+					JArray NewBooks = (JArray)response.Body["items"];
+					foreach (JToken NewBook in NewBooks) {
+						Book book = NewBook.ToObject<Book>();
+						book.ImageUpdate();
+						Books.Add(book);
+						Debug.WriteLine(Books.Count.ToString());
 					}
+					break;
+				}
 				default: {
 					await Application.Current.MainPage.DisplayAlert("Error", response.Body["message"].ToString(), "OK");
 					Page -= 1;
 					break;
 				}
 			}
+			OnPropertyChanged("Books");
 		}
 	}
 }
