68272d1d279fad790be5519a9068c12258de0b38
Gyeongsik Kim <admin@gyungdal.cc>
2019-11-24 20:44:00 +0900

    쓸모없는 ViewCell 랜더러 제거

--- Madome/ViewModels/Library/BookReaderViewModel.cs
+++ Madome/ViewModels/Library/BookReaderViewModel.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using Madome.Struct;
+
 namespace Madome.ViewModels.Library {
 	public class BookReaderViewModel : BaseViewModel {
-		public BookReaderViewModel() {
+		public string[] Items;
+		private readonly Book BookItem;
+		public BookReaderViewModel(Book book) {
+			BookItem = book;
 		}
 	}
 }
--- Madome/Views/Library/BookReader.xaml.cs
+++ Madome/Views/Library/BookReader.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Madome.Struct;
+using Madome.ViewModels.Library;
 using Xamarin.Forms;
 
 namespace Madome.Views.Library {
 	public partial class BookReader : ContentPage {
-		public BookReader() {
+		private readonly BookReaderViewModel viewModel;
+		public BookReader(Book book) {
 			InitializeComponent();
+			viewModel = new BookReaderViewModel(book);
+			BindingContext = viewModel;
 		}
 	}
 }
