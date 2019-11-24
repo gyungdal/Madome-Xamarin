31e348887beb09f5effb9224a66006ad316bf4b8
Gyeongsik Kim <admin@gyungdal.cc>
2019-11-24 09:06:38 +0900

    밑줄 지원용 Label렌더러 추가

---
+++ Madome.Android/Custom/Renderer/UnderLineLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Madome.Custom.Control;
+using Madome.Custom.Control.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(UnderLineLabel), typeof(UnderLineLabelRenderer))]
+namespace Madome.Custom.Control.Droid {
+	public class UnderLineLabelRenderer : LabelRenderer {
+		public UnderLineLabelRenderer(Context context) : base(context) {
+		}
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e) {
+			base.OnElementChanged(e);
+
+			if (Control != null && Element != null) {
+				if (((UnderLineLabel)Element).IsUnderlined) {
+					Control.PaintFlags = PaintFlags.UnderlineText;
+				}
+			}
+		}
+	}
+}
---
+++ Madome/Custom/Control/UnderLineLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Madome.Custom.Control {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public class UnderLineLabel : Label {
+		public static readonly BindableProperty IsUnderlinedProperty =
+			BindableProperty.Create("IsUnderlined", typeof(bool), typeof(UnderLineLabel), false);
+
+		public bool IsUnderlined {
+			get { return (bool)GetValue(IsUnderlinedProperty); }
+			set { SetValue(IsUnderlinedProperty, value); }
+		}
+	}
+}
