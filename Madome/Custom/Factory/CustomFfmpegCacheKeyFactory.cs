a9e9a3140d319a104e813e9b95fcccaad339338f
Kim Gyeongsik <gyungdal@Gyeongsiks-MacBook-Pro-13-2016-no-touchbar.local>
2020-05-21 21:03:01 +0900

    image hash logic change

---
+++ Madome/Custom/Factory/CustomFfmpegCacheKeyFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using FFImageLoading.Forms;
+using Xamarin.Forms;
+
+namespace Madome.Custom.Factory
+{
+	public class CustomFfmpegCacheKeyFactory : ICacheKeyFactory
+    {
+        private string GetHash(string url)
+        {
+            using (HashAlgorithm algorithm = MD5.Create())
+            {
+                byte[] hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(url));
+                return BitConverter.ToString(hash);
+            }
+        }
+        public string GetKey(ImageSource imageSource, object bindingContext)
+        {
+            if (imageSource == null){
+                return null;
+            }
+
+            UriImageSource uriImageSource = imageSource as UriImageSource;
+            if (uriImageSource != null)
+                return GetHash(uriImageSource.Uri.ToString());
+            else
+                throw new NotImplementedException("ImageSource type not supported");
+        }
+    }
+}
